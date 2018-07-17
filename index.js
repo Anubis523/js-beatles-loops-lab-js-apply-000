0ea25360e8b634b2d3a2279b6ecdb46583c49965
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-17 00:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let arr = [];
+  for (let i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[0]} plays ${instruments[0]}.`);
+  }
+  return arr;
+}
